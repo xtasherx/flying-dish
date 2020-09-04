72575c45b51ea36ef0bed826473214e16efe37ec
Tasha <xtasherx@gmail.com>
2020-09-03 22:26:26 -0500

    added code for emailJS call

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-let userSearch = "mango";
+let userSearch = "dessert";
 const searchButton = document.querySelector(".search-btn");
 const searchBar = document.querySelector(".search-bar");
 const cards = document.querySelectorAll(".basic-card");
@@ -7,23 +7,41 @@ const recipeImg = document.querySelectorAll(".recipe-img");
 const recipeSum = document.querySelectorAll(".recipe-sum");
 const recipeIng = document.querySelectorAll(".recipe-ing");
 const recipeInst = document.querySelectorAll(".recipe-inst");
+const siteLink = document.querySelectorAll(".site-url");
+// object for emailjs parameters
+const data = {
+  service_id: "contact_service",
+  template_id: "flyingDish",
+  user_id: "user_N1lY8Mc7IHZz5e2Jv6ZE1",
+  template_params: {
+    email: "xtasherx@gmail.com",
+    title: "I'm a recipe",
+    instructions: "stir",
+    ingredients: "rice",
+    name: "tasha",
+  },
+};
 
 // function to return recipes and write them to the cards
 function getRecipes() {
-  let responseURL = `https://api.spoonacular.com/recipes/complexSearch/?query=${userSearch}&instructionsRequired=true&maxReadyTime=20&addRecipeInformation=true&fillIngredients=true&max-used-ingredients=10&number=6&apiKey=4f924dd683af4b90b667d59fcf07d711`;
+  const responseURL = `https://api.spoonacular.com/recipes/complexSearch/?query=${userSearch}&sort=random&instructionsRequired=true&maxReadyTime=20&addRecipeInformation=true&fillIngredients=true&max-used-ingredients=10&number=6&apiKey=4f924dd683af4b90b667d59fcf07d711`;
   $.ajax({
     url: responseURL,
     method: "GET",
   }).then(function (response) {
     console.log(response);
     cards.forEach(function (card, i) {
+      let instruction = "";
+      let ingredient = "";
+      let emailRecipe = ``;
       titles[i].textContent = `${response.results[i].title}`;
-      recipeSum[i].innerHTML = `${response.results[i].summary}`;
+      recipeSum[
+        i
+      ].innerHTML = `Ready in: ${response.results[i].readyInMinutes} minutes.`;
       recipeImg[i].setAttribute(`src`, `${response.results[i].image}`);
       const instHolder = response.results[i].analyzedInstructions[0].steps;
       const ingrHolder = response.results[i].extendedIngredients;
-      let instruction = "";
-      let ingredient = "";
+
       // loop for instructions array inside response
       instHolder.forEach(function (item) {
         instruction += `${item.step}</br>`;
@@ -33,9 +51,17 @@ function getRecipes() {
         ingredient += `${item.original}</br>`;
       });
 
-      console.log(ingredient);
-      recipeIng[i].innerHTML = instruction;
-      recipeInst[i].innerHTML = ingredient;
+      recipeInst[i].innerHTML = instruction;
+      recipeIng[i].innerHTML = ingredient;
+      siteLink[i].setAttribute(`href`, `${response.results[i].sourceUrl}`);
+      emailRecipe = `
+      <h1> ${response.results[i].title}<h1></br>
+      Ingredient</br>
+      ${ingredient}</br>
+      Instructions</br>
+      ${instruction}
+      `;
+      console.log(emailRecipe);
     });
   });
 }
@@ -47,11 +73,16 @@ searchButton.addEventListener("click", function () {
   getRecipes();
 });
 
-// TO DO /////////////////////////////
+$.ajax("https://api.emailjs.com/api/v1.0/email/send", {
+  type: "POST",
+  data: JSON.stringify(data),
+  contentType: "application/json",
+})
+  .done(function () {
+    alert("Your mail is sent!");
+  })
+  .fail(function (error) {
+    alert("Oops... " + JSON.stringify(error));
+  });
 
-// wire up website button
-//////should go to website for current recipe
-// create ul for igredients and instructions
-// dynamically create lis for both
-// make textContent/innerHTML the current array item
-// append new li
+// TO DO /////////////////////////////
